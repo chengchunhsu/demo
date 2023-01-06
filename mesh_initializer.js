c36b6f19150fca59be75b73319b455f05ea283d5
chengchun.hsu <hsu06118@gmail.com>
2023-01-06 08:04:27 -0600

    update

--- mesh_initializer.js
+++ mesh_initializer.js
@@ -1,5 +1,5 @@
-import * as THREE from './demo/build/three.module.js';
-import { GLTFLoader } from './demo/jsm/GLTFLoader.js';
+import * as THREE from './build/three.module.js';
+import { GLTFLoader } from './jsm/GLTFLoader.js';
 
 
 // dict containing all objects of the scene
